9a5ba909886c1809fb338fd03ebd4144a9a96eb2
Guilherme Galanti <galanti_imp@hotmail.com>
2024-10-30 23:40:03 -0300

    some studies with primitive types and their usage in Rust

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,18 @@
 fn main() {
-    let x:u8 = 10;
+    let x = 1000;
 
-    println!("X = {x}");
+    println!("The number {} has {} bytes", x, std::mem::size_of_val(&x));
 
-    let x:u8 = 2 * x;
+    let decimal: f32 = 2.5;
 
-    println!("x = {x}");
+    println!("The number {} has {} bytes", decimal, std::mem::size_of_val(&decimal));
+
+    // I can't have false as 0 in Rust (what is common in C++)
+    let check = false; // True boolean
+    let check_zero = 0; // Not a bollean
+
+    // Rust uses UTF-8 thats why char has 4 bytes
+    let letter = 'A';
+
+    println!("The number {} has {} bytes", letter, std::mem::size_of_val(&letter));
 }
