ce18b13e6e5bbd4752aaacf3b15a7934bb771cd5
Guilherme Galanti <galanti_imp@hotmail.com>
2024-11-06 00:17:48 -0300

    end match commit

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,11 @@
 fn main() {
-    let multiplicator:u8 = 5;
+    let language = "Kotlin";
+    let purpose = match language {
+        "PHP" => "Web",
+        "Kotlin" => "Android",
+        "Python" => "Data Science",
+        _ => "Inválido"
+    };
 
-    let mut counter:u8 = 0;
-
-    while counter < 10 {
-        counter += 1;
-
-        if counter == 5 { continue };
-
-        println!("{} x {} = {}", multiplicator, counter, multiplicator * counter);
-    }
-
-    counter = 0;
-
-    // Equivalente ao while infinito while true
-    loop {
-        counter +=1;
-
-        println!("{} x {} = {}", multiplicator, counter, multiplicator * counter);
-
-        if counter == 10 {
-            break;
-        }
-    }
-
-    for index in 1..=10 {
-        println!("{} x {} = {}", index, multiplicator, index * multiplicator);
-    }
+    println!("O propósito de {} é {}", language, purpose);
 }
